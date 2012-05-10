a03b2ee9fee656ec7bb45f5d3277ef2935376a17
Gi SC DHS <gisc@dhs.sg>
2012-05-10 13:21:30 +0800

    first commit

---
+++ main.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python
+
+import webapp2	# web application framework
+import jinja2	# template engine
+import os		# access file system
+from google.appengine.api import users	# Google account authentication
+from google.appengine.ext import db		# datastore
+
+# initialize template
+jinja_environment = jinja2.Environment(loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
+
+
+class Contact(db.Expando):
+	''' User data model '''
+	pid = db.StringProperty(required=True)
+	name = db.StringProperty(required=True)
+	email = db.EmailProperty(required=True)
+	
+
+class MainHandler(webapp2.RequestHandler
+	''' Home page handler '''
+	
+    def get(self):
+		''' Show home page '''
+        # check if valid Google account
+		user = users.get_current_user()
+
+		if user:	# if valid logged in user
+			# logout link
+			url = users.create_logout_url(self.request.uri)
+			# logout text
+			url_linktext = 'logout'
+			# retrieve user record
+			query = Contact.gql('WHERE pid = :1', user.nickname())
+			# get 1 record
+			result = query.fetch(1)
+			if result:	# if user record found
+				contact = result[0]
+				greeting = ("Welcome %s!" % (contact.name,))
+			else:		# not found
+				contact = "Invalid dhs.sg user"
+				greeting = "Oops"
+			
+		else: 		# not logged in
+			# login link
+			url = users.create_login_url(self.request.uri)
+			# login text
+			url_linktext = 'login'	
+			contact = "Not authorized"
+			greeting = "You need to"
+		
+		template_values = {
+			'contact': contact,
+			'greeting': greeting,
+			'url': url,
+			'url_linktext': url_linktext
+		}
+		
+# main
+app = webapp2.WSGIApplication([('/', MainHandler)],
+                              debug=True)
+
+							  
+							  
+							  
+							  
+							  
+							  
+							  
+							  
+							  
+							  
+							  
+							  
+							  
+							  
+							  

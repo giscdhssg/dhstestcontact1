c04462d3458ff912118312858392c41f26b7bf5b
Gi SC DHS <gisc@dhs.sg>
2012-05-11 09:18:06 +0800

    added2 bulk upload

---
+++ contact_loader.py
@@ -0,0 +1,20 @@
+﻿from google.appengine.ext import db
+from google.appengine.tools import bulkloader
+
+
+class Contact(db.Expando):
+	''' User data model '''
+	pid = db.StringProperty(required=True)
+	name = db.StringProperty(required=True)
+	email = db.EmailProperty(required=True)
+
+	
+class ContactLoader(bulkloader.Loader):
+    def __init__(self):
+        bulkloader.Loader.__init__(self, 'Contact',
+                                   [('pid', str),
+                                    ('name', str),
+									('email', str)		
+                                   ])
+
+loaders = [ContactLoader]	
